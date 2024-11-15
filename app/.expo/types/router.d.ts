25ebe6c2b49cc8c3ca36ef29c4ff78b286e7986b
ImanZahid <imanzahidzahid@gmail.com>
2024-11-16 00:38:28 +0300

    Fixes

--- app/.expo/types/router.d.ts
+++ app/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/chats` | `/(tabs)/chats/` | `/(tabs)/settings` | `/_sitemap` | `/chats` | `/chats/` | `/login` | `/settings` | `/signup`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/chats` | `/(tabs)/chats/` | `/(tabs)/settings` | `/(tabs)\chats\video-call\[videoCallNumber]` | `/..\store\store` | `/_sitemap` | `/chats` | `/chats/` | `/login` | `/settings` | `/signup`;
       DynamicRoutes: `/(tabs)/chats/${Router.SingleRoutePart<T>}` | `/(tabs)/chats/video-call/${Router.SingleRoutePart<T>}` | `/chats/${Router.SingleRoutePart<T>}` | `/chats/video-call/${Router.SingleRoutePart<T>}`;
       DynamicRouteTemplate: `/(tabs)/chats/[chatId]` | `/(tabs)/chats/video-call/[videoCallNumber]` | `/chats/[chatId]` | `/chats/video-call/[videoCallNumber]`;
     }
