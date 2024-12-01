8992eed9b4044977a60c71f25f6b04e5e965e0d0
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2024-12-01 17:03:34 +0300

    WIP for ML

---
+++ backend/managers/machineLearningTranslationManager.js
@@ -0,0 +1,17 @@
+const socketIo = require('socket.io');
+
+class MachineLearningTranslationManager {
+    constructor(signifyPlusSocketIo) {
+        this.signifyPlusSocketIo = signifyPlusSocketIo;
+        this.setupMachineLearningSocketEvents();
+    }
+
+    setupMachineLearningSocketEvents() {
+       
+    }
+
+}
+
+module.exports = MachineLearningTranslationManager;
+
+
--- backend/managers/websocketManager.js
+++ backend/managers/websocketManager.js
@@ -1,10 +1,12 @@
 const socketIo = require('socket.io');
+const MachineLearningTranslationManager = require('./machineLearningTranslationManager');
 
 class WebSocketManager {
     constructor(server) {
         this.signifyPlusSocketIo = socketIo(server, {
             cors: {origin: "*"}
         });
+        this.machineLearningTranslationManager = MachineLearningTranslationManager(this.signifyPlusSocketIo)
         this.setupSocketEvents();
     }
 
@@ -16,6 +18,32 @@ class WebSocketManager {
                 console.log(message);
                 this.signifyPlusSocketIo.emit('message', `${socket.id.substr(0,2)} said ${message}}`)
             })
+
+            socket.on('offer', (data) => {
+                console.log(`Offer from ${socket.id} to ${data.target}`);
+                socket.to(data.target).emit('offer', {
+                    sender: socket.id,
+                    offer: data.offer
+                });
+            });
+
+            socket.on('answer', (data) => {
+                console.log(`Answer from ${socket.id} to ${data.target}`);
+                socket.to(data.target).emit('answer', {
+                    sender: socket.id,
+                    answer: data.answer
+                });
+            });
+
+            
+            socket.on('ice-candidate', (data) => {
+                console.log(`Ice Candidate from ${socket.id} to ${data.target}`);
+                socket.to(data.target).emit('ice-candidate', {
+                    sender: socket.id,
+                    candidate: data.candidate
+                });
+            });
+
 
             socket.on('disconnect', () => {
                 console.log(`Socket with id ${socket.id.substr(0,2)} disconnected`);
