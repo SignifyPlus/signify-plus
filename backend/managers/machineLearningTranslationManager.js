fa2b1ed8a94e4e637f92e9ca1adb2d85571599ad
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2024-12-02 01:42:36 +0300

    ML class update

--- backend/managers/machineLearningTranslationManager.js
+++ backend/managers/machineLearningTranslationManager.js
@@ -1,4 +1,4 @@
-const socketIo = require('socket.io');
+const axios = require('axios');
 
 class MachineLearningTranslationManager {
     constructor(signifyPlusSocketIo) {
@@ -7,9 +7,28 @@ class MachineLearningTranslationManager {
     }
 
     setupMachineLearningSocketEvents() {
-       
+       this.signifyPlusSocketIo.on('send-frame', async (frame) => {
+            console.log(`Frame Received: `, frame);
+            try {
+                const machineLearningResponse = await this.postFrameToMLEndpoint(frame);
+                this.signifyPlusSocketIo.emit('ml-result', machineLearningResponse);
+            } catch (exception) {
+                console.error('Error processing frame:', exception);
+                this.signifyPlusSocketIo.emit('ml-error', { message: 'Failed to process frame' });
+            }
+       });
     }
 
+    async postFrameToMLEndpoint(frame) {
+        try {
+            const machineLearningEndpoint = ''; //fetch from env variable/or constants
+            const machineLearningResponse = await axios.post(machineLearningEndpoint, frame);
+            return machineLearningResponse.data;
+        } catch (exception) {
+            console.error('Error processing to ML endpoint:', exception);
+            throw new Error("ASL Translation Failed");
+        }
+    }
 }
 
 module.exports = MachineLearningTranslationManager;
