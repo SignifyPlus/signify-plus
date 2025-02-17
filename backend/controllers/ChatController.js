86d90b301791a78112f8b3675fb22298f0dc00e6
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2025-02-17 13:49:41 +0300

    Chat Create endpoint

--- backend/controllers/ChatController.js
+++ backend/controllers/ChatController.js
@@ -1,7 +1,35 @@
 const ServiceFactory = require("../factories/serviceFactory.js");
+const SignifyException = require("../exception/SignifyException.js");
 class ChatController {
     
     constructor(){
+    }
+
+    initializeEmptyChat = async(request, response) => {
+        try {
+            if (request.body.mainUserPhoneNumber === undefined || request.body.targetUserPhoneNumber === undefined) {
+                const signifyException = new SignifyException(400, ` mainUserPhoneNumber or targetUserPhoneNumber not provided!`);
+                return response.status(signifyException.status).json(signifyException.loadResult());
+            }
+
+            const [mainUserPhoneNumberUserObject, targetUserPhoneNumberUserObject] = await Promise.all([
+                ServiceFactory.getUserService.getDocumentByCustomFilters({phoneNumber: request.body.mainUserPhoneNumber}),
+                ServiceFactory.getUserService.getDocumentByCustomFilters({phoneNumber: request.body.targetUserPhoneNumber})]
+            );
+
+            if (mainUserPhoneNumberUserObject == null || targetUserPhoneNumberUserObject == null) {
+                const signifyException = new SignifyException(400, ` mainUserPhoneNumber or targetUserPhoneNumber Users don't exist - please register them first!`);
+                return response.status(signifyException.status).json(signifyException.loadResult());
+            }
+            
+            const [mainUserPhoneNumberId, targetUserPhoneNumberId] = [mainUserPhoneNumberUserObject._id.toString(), targetUserPhoneNumberUserObject._id.toString()]
+            const chat = await ServiceFactory.getChatService.saveDocument({mainUserId: mainUserPhoneNumberId, toUserId: targetUserPhoneNumberId});
+            response.json(chat);
+            
+        }catch(exception) {
+            const signifyException = new SignifyException(500, `Exception Occured: ${exception.message}`);
+            return response.status(signifyException.status).json(signifyException.loadResult());
+        }
     }
     
     getChatByPhoneNumber = async(request, response) => {
@@ -10,7 +38,8 @@ class ChatController {
             const chat = await ServiceFactory.getChatService.getDocumentByCustomFilters({phoneNumber: phoneNumber});
             response.json(chat);
         }catch(exception) {
-            response.status(500).json({error: exception.message})
+            const signifyException = new SignifyException(500, `Exception Occured: ${exception.message}`);
+            return response.status(signifyException.status).json(signifyException.loadResult());
         }
     }
 }
--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const ChatSchema = new mongoose.Schema({
     mainUserId : {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, //User table - foreign key
     toUserId : {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, //User table - foreign key
-    lastMessageId : {type: mongoose.Schema.Types.ObjectId, ref: 'Message', required: true}, //Message table - foreign key
+    lastMessageId : {type: mongoose.Schema.Types.ObjectId, ref: 'Message', required: false}, //Message table - foreign key
 })
 
 const Chat = mongoose.model("Chat", ChatSchema)
