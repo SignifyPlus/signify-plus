0b5b874b45bea7d82f6caa1bdb7838349ac176ff
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2025-02-21 14:56:41 +0300

    More endpoint updates

--- backend/controllers/ChatController.js
+++ backend/controllers/ChatController.js
@@ -8,20 +8,31 @@ class ChatController {
 
     initializeEmptyChat = async(request, response) => {
         try {
+            //request validation
+            const mainUserPhoneNumberValidation = await ExceptionHelper.validate(request.body.mainUserPhoneNumber, 400, `mainUserPhoneNumber is not provided.`, response);
+            if (mainUserPhoneNumberValidation) return mainUserPhoneNumberValidation;
+ 
             const participants = await ExceptionHelper.validate(request.body.participants, 400, `participants array not provided. Please add the participants that will be participate in a chat - participants : [+905232314, +9023132145]`, response);
             if (participants) return participants;
-            const particpantsUserObjects = await ServiceFactory.getUserService.getDocumentsByCustomFilters({phoneNumber: { $in: request.body.participants } })
 
+            //main User table validation
+            const mainUserPhoneNumberUserObject = await ServiceFactory.getUserService.getDocumentByCustomFilters({phoneNumber: request.body.mainUserPhoneNumber});
+            const mainUserObjectValidation = await ExceptionHelper.validate(mainUserPhoneNumberUserObject, 400, `mainUserPhoneNumber doesnt Exist in the user table!`, response);
+            if (mainUserObjectValidation) return mainUserObjectValidation;
+
+            //participants validation
+            const particpantsUserObjects = await ServiceFactory.getUserService.getDocumentsByCustomFilters({phoneNumber: { $in: request.body.participants } })
             if (particpantsUserObjects.length != request.body.participants.length) {
                 const signifyException = new SignifyException(400, `Not all phoneNumbers are registered to the User table - can't create a chat`);
                 return response.status(signifyException.status).json(signifyException.loadResult());
             }
-            
+            //create chat document/object
             const chat = await ServiceFactory.getChatService.saveDocument({
+                mainUserId: mainUserPhoneNumberUserObject._id.toString(),
                 participants: particpantsUserObjects.map(participant => participant._id.toString())
             });
-            response.json(chat);
-            
+
+            return response.json(chat);
         }catch(exception) {
             const signifyException = new SignifyException(500, `Exception Occured: ${exception.message}`);
             return response.status(signifyException.status).json(signifyException.loadResult());
@@ -39,5 +50,22 @@ class ChatController {
             return response.status(signifyException.status).json(signifyException.loadResult());
         }
     }
+
+    getChatHistoryById = async(request, response) => {
+        try {
+            const chatIdValidation = await ExceptionHelper.validate(request.params.chatId, 400, `chatId is not provided.`, response);
+            if (chatIdValidation) return chatIdValidation;
+
+            //now us this chat id to query on messages table and retrieve the entire history
+            const chat = await ServiceFactory.getChatService.getDocumentByCustomFilters({phoneNumber: phoneNumber});
+
+            
+            response.json(chat);
+        }catch(exception) {
+            const signifyException = new SignifyException(500, `Exception Occured: ${exception.message}`);
+            return response.status(signifyException.status).json(signifyException.loadResult());
+        }
+    }
+
 }
 module.exports = ChatController;
