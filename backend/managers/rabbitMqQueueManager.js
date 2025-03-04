080fbc5f363d5eb743de0ea40b830eea637fc003
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2025-03-04 13:16:23 +0300

    RabbitMq Updates

--- backend/managers/rabbitMqQueueManager.js
+++ backend/managers/rabbitMqQueueManager.js
@@ -11,12 +11,58 @@ class RabbitMQQueueManager {
     async establishConnection() {
         try{
             this.#rabbitMqConnection = await amqp.connect(this.rabbitMqQueueURL);
-            console.log(`Connection to RabbitMQ established!`);
-
             this.#rabbitMqChannel = await this.#rabbitMqConnection.createChannel();
-            console.log(`Channel Created!`);
         }catch(exception){
             console.log(`Error: ${exception}`);
+            throw exception;
+        }
+    }
+
+    async disposeConnection () {
+        try {
+            if (this.#rabbitMqChannel) {
+                await this.#rabbitMqChannel.close();
+            }
+
+            if (this.#rabbitMqConnection) {
+                await this.#rabbitMqConnection.close();
+            }
+
+        }catch(exception) {
+            console.log(`Error: ${exception}`);
+            throw exception;
+        }
+    }
+
+    async queueMessage(queueName, message) {
+        try {
+            //message survives when persistent is set to true
+            //durable : true, queue survives upon crash/restart
+            await this.#rabbitMqChannel.assertQueue(queueName, {durable: true});
+            this.#rabbitMqChannel.sendToQueue(queueName, Buffer.from(message), {persistent: true});
+            console.log(`Message has been queued!`);
+        }catch(exception) {
+            console.log(`Exception Occured: ${exception}`);
+            throw exception;
+        }
+    }
+
+    async popMessage(queueName) {
+        try {
+            await this.#rabbitMqChannel.assertQueue(queueName, {durable: true});
+            return new Promise((resolve, reject) => {
+                this.#rabbitMqChannel.consume(queueName, (message) => {
+                    if (message) {
+                        //sends acknowledgement that its ready to be consumed - hence dequeues it entirely from the queue
+                        this.#rabbitMqChannel.ack(message);
+                        resolve(message.content.toString());
+                    }
+                    reject(new Error(`No Message Available`));
+                });
+            })
+        }catch(exception) {
+            console.log(`Exception Occured: ${exception}`);
+            throw exception;
         }
     }
 
