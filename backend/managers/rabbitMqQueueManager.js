f4d4d47232e0c01ae9fcf1d6821fd7368db27cef
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2025-03-06 16:40:55 +0300

    Small update

--- backend/managers/rabbitMqQueueManager.js
+++ backend/managers/rabbitMqQueueManager.js
@@ -47,32 +47,6 @@ class RabbitMQQueueManager {
         }
     }
 
-    //generator which fields the result continuosly - indefinitely runs
-    async *popMessages(queueName) {
-        var consumerTag;
-        try {
-            await this.#rabbitMqChannel.assertQueue(queueName, {durable: true});
-            consumerTag = await this.#rabbitMqChannel.consume(queueName, (message) => {
-                    if (message) {
-                        //sends acknowledgement that its ready to be consumed - hence dequeues it entirely from the queue
-                        this.#rabbitMqChannel.ack(message);
-                        yield JSON.parse(message.content.toString());
-                    }
-                });
-        }catch(exception) {
-            console.log(`Exception Occured: ${exception}`);
-            await this.haltConsumer(consumerTag);
-            throw new Error(`${exception}`);
-        }
-    }
-
-    async haltConsumer(consumerTag) {
-        if (consumerTag == null) {
-            throw new Error(`Tried to close a consumer tag which is null!`);
-        }
-        await this.#rabbitMqChannel.cancel(consumerTag);
-    }
-
     async getRabbitMqConnection() {
         return this.#rabbitMqConnection
     }
--- backend/processors/rabbitMqMessageProcessor.js
+++ backend/processors/rabbitMqMessageProcessor.js
@@ -1,13 +1,31 @@
 const ControllerFactory = require("../factories/controllerFactory.js");
 const ManagerFactory = require("../factories/managerFactory.js");
 class RabbitMqMessageProcessor{
+    #dequeuedMessages = []
     constructor() {}
+    async messageProcessor(queueName) {
+        var consumerTag;
+        try {
+            await ManagerFactory.getRabbitMqQueueManager().getRabbitMqChannel().assertQueue(queueName, {durable: true});
+            consumerTag = ManagerFactory.getRabbitMqQueueManager().getRabbitMqChannel().consume(queueName, (message) => {
+                    if (message) {
+                        ManagerFactory.getRabbitMqQueueManager().getRabbitMqChannel().ack(message);
+                        this.#dequeuedMessages.push(JSON.parse(message.content.toString()));
+                    }
+                }, {noAck: false});
 
-    async consumeMessagesFromRabbitMqQueue(queueName) {
-        for await (const message of ManagerFactory.getRabbitMqQueueManager().popMessages(queueName)) {
-            //preprocess message and push
-            
+        }catch(exception) {
+            console.log(`Exception Occured: ${exception}`);
+            await this.haltConsumer(consumerTag);
+            throw new Error(`${exception}`);
         }
+    }
+
+    async haltConsumer(consumerTag) {
+        if (consumerTag == null) {
+            throw new Error(`Tried to close a consumer tag which is null!`);
+        }
+        await ManagerFactory.getRabbitMqQueueManager().getRabbitMqChannel().cancel(consumerTag);
     }
 }
 
