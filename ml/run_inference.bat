233bdd34b1e94193e86cbfb2ce330bb39a2495a3
Meskaz285 <meskaz285@gmail.com>
2025-03-08 18:06:42 +0300

    remove unused files

--- ml/run_inference.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Activate the inferencevenv virtual environment
-call ml\inferencevenv\Scripts\activate.bat
-
-rem Run the testtestONNX.py script
-python ml\testtestONNX.py
-
-rem Keep the terminal open to see the output
-pause
