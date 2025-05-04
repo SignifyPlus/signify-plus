9859dbb6f05a87290ea84d0e9983534c3f9cf97b
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2025-05-04 17:07:03 +0300

    Small Update

--- backend/middlewares/authMiddleWare.js
+++ backend/middlewares/authMiddleWare.js
@@ -11,6 +11,7 @@ class AuthMiddleWare {
          AuthConstants.JWT_REFRESH,
          AuthConstants.TWILIO_GET,
          AuthConstants.TWILIO_VERIFY,
+         AuthConstants.UPDATE_USER
       ];
    }
 
--- backend/middlewares/constants/authConstants.js
+++ backend/middlewares/constants/authConstants.js
@@ -4,6 +4,8 @@ class AuthConstants {
    static JWT_REFRESH = '/jwt/refresh';
    static AUTHORIZATION_HEADER = 'authorization';
    static TWILIO_VERIFY = '/twilio/verifyOtp';
+   static UPDATE_USER = '/users/update';
+
    static TWILIO_GET = '/twilio/getOtp';
    static FIRST = 1;
    static UNAUTHORIZED = 401;
