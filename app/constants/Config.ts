ceb1da704645b38843f546fc786d35ffb68371eb
Meskaz285 <meskaz285@gmail.com>
2025-05-07 02:47:55 +0300

    Final Touches

--- app/constants/Config.ts
+++ app/constants/Config.ts
@@ -1,5 +1,6 @@
 //export const API_URL = 'https://signify-plus.onrender.com';
-export const API_URL = 'http://139.179.232.90:3001';
-//export const API_URL = 'https://signify-plus-production.up.railway.app';
+//export const API_URL = 'http://139.179.232.90:3001';
+export const API_URL = 'https://signify-plus-production.up.railway.app';
 export const NGROK_URL = 'https://moving-cardinal-happily.ngrok-free.app/meeting-id';
 export const ML_WEBSOCKET_URL = 'ws://139.179.232.90:8080/ws/react';
+
